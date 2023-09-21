bee861bdd43d231db1aae464f3252d8ebb998f27
Yusha Arif <101613943+YushaArif99@users.noreply.github.com>
2023-09-21 05:26:14 +0000

    (chore)(cfe): did some code cleanups for the convert_operators.py which contained quite a few unneccessary operations

--- control_flow_experimental/dygraph_ivy/dy2static/__init__.py
+++ control_flow_experimental/dygraph_ivy/dy2static/__init__.py
@@ -1,12 +1,7 @@
 from .utils import saw, UndefinedVar, ast_to_source_code
-from .convert_operators import convert_logical_and as And  # noqa: F401
 from .convert_operators import convert_assert as Assert  # noqa: F401
 from .convert_call_func import convert_call as Call  # noqa: F401
 from .convert_operators import convert_len as Len  # noqa: F401
-from .convert_operators import convert_logical_not as Not  # noqa: F401
-from .convert_operators import convert_logical_or as Or  # noqa: F401
-#from .convert_operators import convert_pop as Pop  # noqa: F401
-from .convert_operators import convert_shape as Shape  # noqa: F401
 from .convert_operators import unpack_by_structure as Unpack  # noqa: F401
 from .convert_operators import indexable as Indexable  # noqa: F401
 from .convert_operators import cmp_eq, cmp_ne, cmp_gt, cmp_ge, cmp_lt, cmp_le
