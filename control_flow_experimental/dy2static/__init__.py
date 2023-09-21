3f1a5e50911383a25fceabb79b628569b830575a
Yusha Arif <101613943+YushaArif99@users.noreply.github.com>
2023-09-21 12:00:32 +0000

    (fix)(CI tests): fixed circular import issues caused by the addition of Proxy import in the convert_operators.py file in dygraph API

--- control_flow_experimental/dy2static/__init__.py
+++ control_flow_experimental/dy2static/__init__.py
@@ -6,7 +6,8 @@ from .convert_operators import unpack_by_structure as Unpack  # noqa: F401
 from .convert_operators import indexable as Indexable  # noqa: F401
 from .convert_operators import cmp_eq, cmp_ne, cmp_gt, cmp_ge, cmp_lt, cmp_le
 from .convert_operators import cmp_is, cmp_isnot, cmp_in, cmp_notin
-from .convert_operators import bool_and, bool_or, unary_not, convert_to_f_string
+from .convert_operators import bool_and, bool_or, unary_not
+from .convert_operators import convert_to_f_string, transform_funcs
 from .convert_operators import cast_to_bool, cast_to_float, cast_to_int
 from .convert_operators import cast_to_dict, cast_to_set, cast_to_tuple, cast_to_list
 from .assert_transformer import AssertTransformer
